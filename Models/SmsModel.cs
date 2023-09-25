68a96f94cdd6380da607fa4b5fcf07867979b20d
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-09-25 10:50:39 +0530

    Add files via upload

---
+++ Models/SmsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Twilio_4_project.Models
+{
+    public class SmsModel
+    {
+        public string PhoneNumbers { get; set; }
+        public string Message { get; set; }
+    }
+
+}
